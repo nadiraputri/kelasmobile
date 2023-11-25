7ebfa002ff1bf82393a050d943c667febdea5319
nadirahijriani26 <nadirahijriani26@gmail.com>
2023-11-26 00:18:19 +0700

    upload lagi

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,53 @@
-import React, {useState, useEffect} from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {View, Text} from 'react-native';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 
-const Menu = () => {
+const Quran = () => {
+    const [dataQuran, setDataQuran] = useState(null);
+    //2. jalankan fungsi getQuran
+    useEffect(() => {
+        getQuran();
+    });
+    //1. ambil data dari    API
+    const getQuran = async () => {
+        try {
+            const response = await fetch('https://equran.id/api/v2/surat');
+            const data = await response.json();
+            setDataQuran(data.data);
+            console.log(data.data);
+        }   catch (error) {
+            console.log(error);
+        }
+    };
+
+
   return (
-    <View style={{flex: 1, backgroundColor: '#FFDEAD', alignItems:'center'}}>
-      <Text style={{fontSize: 22, color: '#000000'}}>Studio Foto</Text>
+    <View style={styles.box}>
+      <Text>Quran</Text>
+      <ScrollView>
+        {dataQuran?.map((data, index) =>  {
+          return (
+            <TouchableOpacity key={index}>
+              <Text style={styles.text}>
+                {data.nomor} - {data.nama}
+              </Text>
+            </TouchableOpacity>
+          );
+         }) }
+      </ScrollView>
     </View>
-  );
-};
+  )
+}
 
-export default Menu;
+const styles = StyleSheet.create({
+    box: {
+        padding: 15,
+        flex: 1,
+        backgroundColor: '#FFDEAD',
+      },
+      text: {
+        fontSize: 20,
+        color: 'black'
+      },
+});
+
+export default Quran;
